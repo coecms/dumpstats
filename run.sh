c15f613721df5ffdc96aabd40ed92a78e447e08b
Aidan Heerdegen <aidan.heerdegen@anu.edu.au>
2019-09-13 11:27:58 +1000

    Add upload tasks to run script

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ git pull
 # Run doit with the default dodo.py script. Use a bunch
 # of threads to speed it up. Select just the dump_SU and
 # dump_storage tasks
-doit -n 64 -P thread dump_SU dump_storage
+doit -n 64 -P thread dump_SU dump_storage start_tunnel upload_storage upload_usage
